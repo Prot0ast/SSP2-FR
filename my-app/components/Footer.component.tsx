6b66bb037ab719e4188ba06d62f69994ad946fae
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-18 11:33:05 -0500

    Removing project and trying again

--- my-app/components/Footer.component.tsx
+++
@@ -1,9 +0,0 @@
-import "./Component.css";
-
-export function Footer() {
-  return (
-    <footer className="footer">
-        <span>Copyright 2024 Quantum Com LLC *not a real company*</span>
-    </footer>
-  );
-}
--- my-app/components/index.ts
+++
@@ -1,6 +0,0 @@
-import { Footer } from "./Footer.component";
-import { Header } from "./Header.component";
-import { CustomerTableRow } from "./CustomerTableRow";
-import { PlanTableRow } from "./PlanTableRow";
-
-export { Footer, Header, CustomerTableRow, PlanTableRow};
